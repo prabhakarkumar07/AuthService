86b2509469922d7d7d518536f2d10fb8a643897d
prabhakarkumar07 <prabhakarkumarekc477@gmail.com>
2025-11-29 00:42:37 +0530

    Removed Lombok annotations and fixed duplicate constructors

--- src/main/java/com/prabhakar/auth/model/User.java
+++ src/main/java/com/prabhakar/auth/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
+	//Removed Lombok annotations and fixed duplicate constructors
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
